cde855903a1722e4c33a612aac5efe3054c50f4f
Yu357 <forestgreen14689@gmail.com>
2022-05-22 19:53:53 +0900

    feat: ルーティングを設定

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import Top from './screens/Top'
 import Gallery from './screens/Gallery'
 import Contact from './screens/Contact'
-
+import NotFound from './screens/NotFound'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Top />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Top/>}/>
+        <Route path='/gallery' element={<Gallery/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
