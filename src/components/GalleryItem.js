0fbd4669fbab6333ed63a68d4afddb111b3be8d7
Yu357 <forestgreen14689@gmail.com>
2022-05-22 22:06:19 +0900

    feat: Galleryページのコンテンツを表示

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,11 @@
+
+function GalleryItem(props) {
+  return (
+    <div className="idea-item">
+      <a href={props.href}><img src={props.photo} alt={props.alt}/></a>
+      <a href={props.href}>{props.title}</a>
+    </div>
+  )
+}
+
+export default GalleryItem
