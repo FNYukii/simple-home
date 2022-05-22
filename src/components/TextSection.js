403bef78b94c431996a4c00b5870d45f04ba0f7e
Yu357 <forestgreen14689@gmail.com>
2022-05-22 21:17:41 +0900

    Reactコンポーネント読み込み

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react'
-
 function TextSection(props) {
 
   return (
-    <div>
-
+    <div className="section">
+      <div >
+        <img src={props.photo} className="trigger-to-popup-modal" alt=''/>
+      </div>
+      <div className="small-container">
+        <h2>Hello</h2>
+        <p style={{whiteSpace: 'pre-line'}}>{props.text}</p>
+      </div>
     </div>
   )
 }
