e51e351fefd5bfe9f6716f518bf1e8e89046b4d1
Yu357 <forestgreen14689@gmail.com>
2022-05-23 11:52:13 +0900

    feat: html要素を配置

--- src/screens/Thanks.js
+++ src/screens/Thanks.js
@@ -1,7 +1,9 @@
 function Thanks() {
   return(
     <main>
-      <h2>Thanks</h2>
+      <h2>Thanks for your message.</h2>
+      <p>Voluptatem quod nostrum numquam dicta quidem! Sit amet consectetur adipisicing.</p>
+      <a href='/'>Top</a>
     </main>
   )
 }
