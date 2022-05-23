a90aa0859e357e8bf6b841b616d07af61d9e1555
Yu357 <forestgreen14689@gmail.com>
2022-05-23 23:31:17 +0900

    feat: NotFoundページ作成

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -4,7 +4,17 @@ function NotFound() {
 
   return (
     <main>
-      <h2>NotFound</h2>
+      <div className='large-container'>
+        
+        <div className='message-section'>
+          <div>
+            <h2>Page not found</h2>
+            <p>Voluptatem quod nostrum numquam dicta quidem! Sit amet consectetur adipisicing.</p>
+            <a href='/'>Top</a>
+          </div>
+        </div>
+        
+      </div>
     </main>
   )
 }
