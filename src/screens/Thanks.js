ba4f33897e65e2b87913799d2c669e4563dc69da
Yu357 <forestgreen14689@gmail.com>
2022-05-23 12:13:05 +0900

    feat: Thanksページのコンテンツを画面中央に配置

--- src/screens/Thanks.js
+++ src/screens/Thanks.js
@@ -3,10 +3,12 @@ import '../styles/thanks.css'
 function Thanks() {
   return(
     <main className='thanks-page'>
-      <h2>Thanks for your message</h2>
-      <p>Voluptatem quod nostrum numquam dicta quidem! Sit amet consectetur adipisicing.</p>
-      <a href='/'>Top</a>
-
+      <div>
+        <h2>Thanks for your message</h2>
+        <p>Voluptatem quod nostrum numquam dicta quidem! Sit amet consectetur adipisicing.</p>
+        <a href='/'>Top</a>
+        
+      </div>
     </main>
   )
 }
