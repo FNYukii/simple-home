3a02f2b848b9dbfaf90b66310a21157f8f850702
Yu357 <forestgreen14689@gmail.com>
2022-05-24 14:58:54 +0900

    feat: Reactの条件付きレンダーを試した

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,53 @@
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import '../styles/header.css'
-import { FaBars, FaTimes, FaMoon } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Header() {
+
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+  const onTapBars = () => {
+    setIsOpenMenu(true)
+    alert('Open')
+  }
+
+  const onTapTimes = () => {
+    setIsOpenMenu(false)
+    alert('Close')
+  }
+
   return (
+
     <header>
       <div className='large-container'>
         <a href='/' className='logo'>Simple Home</a>
 
-        <FaBars className='bars'/>
+        <FaBars className='bars' onClick={onTapBars}/>
 
-        <ul className='global-nav'>
-          <li><NavLink to='/' activeclassname='active' exact='true'>Top</NavLink></li>
-          <li><NavLink to='/gallery' activeclassname='active'>Gallery</NavLink></li>
-          <li><NavLink to='/contact' activeclassname='active'>Contact</NavLink></li>
-        </ul>
+        {!isOpenMenu &&
+          <ul className='global-nav'>
+            <li><NavLink to='/' activeclassname='active' exact='true'>Top</NavLink></li>
+            <li><NavLink to='/gallery' activeclassname='active'>Gallery</NavLink></li>
+            <li><NavLink to='/contact' activeclassname='active'>Contact</NavLink></li>
+          </ul>
+        }
 
       </div>
 
-      <div className='hamburger-menu'>
-        <FaTimes/>
+      {isOpenMenu &&
+        <div className='hamburger-menu'>
+          <FaTimes onClick={onTapTimes}/>
 
-        <ul>
-          <li><a href="index.html">Top</a></li>
-          <li><a href="gallery.html">Gallery</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
+          <ul>
+            <li><a href="index.html">Top</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
 
-        <div>
-          <FaMoon/>
         </div>
-
-      </div>
-
+      }
+      
     </header>
   )
 }
