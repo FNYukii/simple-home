d0d4ab4509580ccdfbb55a27fdcbf1ce30a387d7
Yu357 <forestgreen14689@gmail.com>
2022-05-25 21:13:17 +0900

    feat: TextSectionのスタイルもモジュール化

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -1,14 +1,14 @@
-import '../styles/section.css'
+import styles from '../styles/textSection.module.css'
 
 function TextSection(props) {
 
   return (
-    <div className='section'>
-      <div className={props.containerClassName}>
-        <img src={props.photo} className='trigger-to-popup-modal' alt=''/>
+    <div className={styles.textSection}>
+      <div className={props.isLarge ? styles.largeContainer : styles.smallContainer}>
+        <img className={styles.image} src={props.photo} alt=''/>
       </div>
 
-      <div className='small-container'>
+      <div className={styles.smallContainer}>
         <h2>{props.title}</h2>
         <p style={{whiteSpace: 'pre-line'}}>{props.text}</p>
       </div>
