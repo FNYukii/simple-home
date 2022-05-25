1836c230b4c7ad86b3b36fe3f8584d6bc11b9c89
Yu357 <forestgreen14689@gmail.com>
2022-05-25 22:59:04 +0900

    feat: IdeaをCSS Module化

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -1,8 +1,8 @@
-import '../styles/idea.css'
+import styles from '../styles/idea.module.css'
 
 function Idea(props) {
   return (
-    <div className='idea'>
+    <div className={styles.idea}>
       <a href={props.href}><img src={props.photo} alt={props.alt}/></a>
 
       <a href={props.href}>{props.title}</a>
