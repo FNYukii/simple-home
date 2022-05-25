c99c5b2c7d98bfd7717a3ca69c9d87212b8c4e23
Yu357 <forestgreen14689@gmail.com>
2022-05-25 15:52:29 +0900

    feat: GalleryItemのスタイル調整

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -2,7 +2,7 @@ import '../styles/galleryItem.css'
 
 function GalleryItem(props) {
   return (
-    <div className='idea-item'>
+    <div className='gallery-item'>
       <a href={props.href}><img src={props.photo} alt={props.alt}/></a>
 
       <a href={props.href}>{props.title}</a>
